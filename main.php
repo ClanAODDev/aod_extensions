a3ff3f8b38dec3065d4653f976a7f03108dc88e2
Chris Deaton <dcdeaton@ncsu.edu>
2016-10-07 16:47:04 -0400

    initial

---
+++ main.php
@@ -0,0 +1,15 @@
+<?php
+/*
+Plugin Name: ClanAOD Extensions
+Plugin URI:  https://github.com/flashadvocate/ClanAOD_Extensions
+Description: Extended functionality for ClanAOD Website WP implementation
+Version:     0.1.0
+Author:      ClanAOD
+Author URI:  https://clanaod.net/
+*/
+
+defined('ABSPATH') or die();
+
+require __DIR__ . '/vendor/autoload.php';
+
+new \ClanAOD\Extensions();
