4337f9d9b132b13a0a58e6935cd9b149ecc43c5c
Chris Deaton <dcdeaton@ncsu.edu>
2016-10-07 16:56:06 -0400

    update commenting

--- main.php
+++ main.php
@@ -1,12 +1,12 @@
 <?php
 /*
-Plugin Name: ClanAOD Extensions
-Plugin URI:  https://github.com/flashadvocate/ClanAOD_Extensions
-Description: Extended functionality for ClanAOD Website WP implementation
-Version:     0.1.0
-Author:      ClanAOD
-Author URI:  https://clanaod.net/
-*/
+ * Plugin Name: ClanAOD Extensions
+ * Plugin URI:  https://github.com/flashadvocate/ClanAOD_Extensions
+ * Description: Extended functionality for ClanAOD Website WP implementation
+ * Version:     0.1.0
+ * Author:      ClanAOD
+ * Author URI:  https://clanaod.net/
+ */
 
 defined('ABSPATH') or die();
 
