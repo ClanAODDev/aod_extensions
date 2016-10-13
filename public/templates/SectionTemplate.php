e2746f1f3f469a734b04215b001f5e2c29e95828
Chris Deaton <dcdeaton@ncsu.edu>
2016-10-12 23:18:05 -0400

    fix short-width

--- public/templates/SectionTemplate.php
+++ public/templates/SectionTemplate.php
@@ -8,7 +8,7 @@
     <div class="content-container <?= $centerContent ?>">
         <h1><?= $attr['section_title'] ?></h1>
         <?php if ( ! empty($sectionImage)): ?>
-            <p class="short-width"><?= $content ?></p>
+            <div class="short-width"><?= $content ?></div>
         <?php else: ?>
             <p><?= $content ?></p>
         <?php endif; ?>
