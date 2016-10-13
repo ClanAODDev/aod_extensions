f474e1ca02f6d488bd6255448cb1a608545905e2
Chris Deaton <dcdeaton@ncsu.edu>
2016-10-12 23:51:41 -0400

    add image to section template

--- public/templates/SectionTemplate.php
+++ public/templates/SectionTemplate.php
@@ -8,9 +8,18 @@
     <div class="content-container <?= $centerContent ?>">
         <h1><?= $attr['section_title'] ?></h1>
         <?php if ( ! empty($sectionImage)): ?>
-            <div class="short-width"><p><?= wpautop($content) ?></p></div>
+            <div class="short-width">
+                <div class="blurb">
+                    <?= wpautop($content) ?>
+                </div>
+                <div class="section-image">
+                    <?= $sectionImage ?>
+                </div>
+            </div>
         <?php else: ?>
-            <?= wpautop($content) ?>
+            <div class="blurb">
+                <?= wpautop($content) ?>
+            </div>
         <?php endif; ?>
     </div>
 </section>
