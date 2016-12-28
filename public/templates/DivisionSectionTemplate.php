27cc0ba77020b09084febb252cebe2c7fad1d9fc
Chris Deaton <dcdeaton@ncsu.edu>
2016-12-28 16:27:14 -0500

    add class flag for automenu

--- public/templates/DivisionSectionTemplate.php
+++ public/templates/DivisionSectionTemplate.php
@@ -8,7 +8,8 @@ use ClanAOD\Helpers;
 ?>
 <div class="section-sub-section">
     <?php if ( ! empty($attr['section_title'])): ?>
-        <h2 id="<?= Helpers::anchored($attr['section_title']); ?>"><?= $attr['section_title'] ?></h2>
+        <h2 class="automenu-heading"
+            id="<?= Helpers::anchored($attr['section_title']); ?>"><?= $attr['section_title'] ?></h2>
     <?php endif; ?>
     <?= wpautop($content) ?>
 </div>
