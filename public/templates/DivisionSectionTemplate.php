1d09539ef1681fe39d0c24442b0e5a4931a84acd
Chris Deaton <dcdeaton@ncsu.edu>
2016-12-31 11:11:28 -0500

    automenu in container

--- public/templates/DivisionSectionTemplate.php
+++ public/templates/DivisionSectionTemplate.php
@@ -6,10 +6,9 @@
 use ClanAOD\Helpers;
 
 ?>
-<div class="section-sub-section">
+<div class="section-sub-section automenu">
     <?php if ( ! empty($attr['section_title'])): ?>
-        <h2 class="automenu-heading"
-            id="<?= Helpers::anchored($attr['section_title']); ?>"><?= $attr['section_title'] ?></h2>
+        <h2 id="<?= Helpers::anchored($attr['section_title']); ?>"><?= $attr['section_title'] ?></h2>
     <?php endif; ?>
     <?= wpautop($content) ?>
 </div>
